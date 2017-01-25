e40d15fad8506896f8bb76cc6582aa973a6f65fc
Jun Masuda <kibajun29@hotmail.co.jp>
2017-01-25 16:59:00 +0000

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123033936) do
+ActiveRecord::Schema.define(version: 20170125165523) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "area"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
