4a03bbe05d4938e0d34101c07e7338bca43329bd
Jun Masuda <kibajun29@hotmail.co.jp>
2017-01-25 17:09:10 +0000

    add user model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,5 +5,7 @@ class Profile < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  profile
+  area
   has_secure_password
 end
