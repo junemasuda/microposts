ffec7eb6a8d44c4cd453e94c8758328a22676653
Jun Masuda <kibajun29@hotmail.co.jp>
2017-01-26 03:11:19 +0000

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
+  get    'users/:id/edit', to: 'users#edit'
+  patch  'users/:id/edit', to: 'users#update'
+  put    'users/:id/edit', to: 'users#update'
+  delete 'users/:id/edit', to: 'users#destroy'
+  
   resources :users
 end
