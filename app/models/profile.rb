52a77ff0c9df4a6c756af223f83dbbb54573ea7d
Jun Masuda <kibajun29@hotmail.co.jp>
2017-02-08 09:01:13 +0000

    kadai3

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,7 +5,6 @@ class Profile < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  profile
   area
   has_secure_password
 end
